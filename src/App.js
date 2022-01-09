e0641f99bcc92b75551386468e3d054fc192f6ec
teerapop67 <52346089+teerapop67@users.noreply.github.com>
2022-01-09 22:20:55 +0700

    fixed again

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React, { useState, useMemo, useRef } from 'react';
+import React, { useState, useMemo, useRef, useCallback } from 'react';
 import Grid from './components/Grid';
 import ColorPicker from './components/ColorPicker';
 import useStyles from './App.styles';
